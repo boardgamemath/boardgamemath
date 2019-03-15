087f4dbec262428c59416e21407ddb4a999af25c
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2019-03-15 21:04:04 +0100

    selected cards

--- boardgamemath-website/boardgames/gloomhaven/gloomhavenDeckBuilder.js
+++ boardgamemath-website/boardgames/gloomhaven/gloomhavenDeckBuilder.js
@@ -2,6 +2,10 @@ var urlSearchParams = new URLSearchParams(window.location.search);
 
 var characters;
 var selectedCharacter;
+var availableCards;
+var selectedCards;
+
+var availableCardDiv;
 
 initCharacters();
 
@@ -10,10 +14,56 @@ function initCharacters() {
         characters = data.characters;
         console.log(characters);
         selectedCharacter = characters[0];
-        d3.select("select").selectAll("option").data(characters)
+        d3.select("select").selectAll("option")
+                .data(characters)
                 .enter().append("option").attr("value",function(character){ return character;})
                 .text(function(character){
                     return character.characterName;
                 });
+        availableCards = selectedCharacter.cards;
+        selectedCards = [];
+        availableCardDiv = d3.select(".availableCards").selectAll(".availableCard")
+                .data(availableCards)
+                .enter().append("div")
+                .attr("class", "availableCard")
+                .on("click", function (availableCard) {
+                    if (availableCard.selected) {
+                        availableCard.selected = false;
+                        var index = selectedCards.indexOf(availableCard);
+                        selectedCards.splice(index, 1);
+                        selectedCards.push(availableCard);
+                    } else {
+                        availableCard.selected = true;
+                        selectedCards.push(availableCard);
+                    }
+                    updateCards();
+                });
+        availableCardDiv.append("img")
+                .attr("src",  function (availableCard) {
+                    return "deck/" + selectedCharacter.characterId + "/"
+                    + selectedCharacter.characterId + "-" + availableCard.cardId + ".png"
+                });
+        d3.select(".selectedCards").selectAll(".selectedCard")
+                .data(selectedCards)
+                .enter().append("div")
+                .attr("class", "selectedCard")
+                .append("img")
+                .attr("src",  function (selectedCard) {
+                    return "deck/" + selectedCharacter.characterId + "/"
+                            + selectedCharacter.characterId + "-" + selectedCard.cardId + ".png"
+                });
+        updateCards();
     });
 }
+
+function changeCharacter(t) {
+    selectedCharacter = characters[t.selectedIndex];
+    updateCards();
+}
+
+function updateCards() {
+    availableCardDiv.classed("selectedAvailableCard", function (availableCard) {
+        return availableCard.selected
+    })
+}
+
